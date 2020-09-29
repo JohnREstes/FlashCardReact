1f218a34d56296d1cb1c0c63724f0b9647d7ab8d
John R. Estes <John.R.Estes@icloud.com>
2020-09-29 19:00:12 -0400

    new modal that works!

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 import BuildCardStack from './components/buildCardStack.js';
 import BuildCard from './components/buildCard.js';
-import Modal from './components/modal.js';
+import Modal from 'react-awesome-modal';
 
 class App extends Component {
 
@@ -12,7 +12,7 @@ class App extends Component {
     this.state = {
       flashCards:[],
       loading: true, 
-      isShowing: false
+      visible : false
     }
 
   }
@@ -25,39 +25,39 @@ class App extends Component {
     }));
   }
 
-  openModalHandler = () => {
+  openModal() {
     this.setState({
-        isShowing: true
+        visible : true
     });
-}
+  }
 
-    closeModalHandler = () => {
-        this.setState({
-            isShowing: false
-        });
-    }
+  closeModal() {
+      this.setState({
+          visible : false
+      });
+  }
 
   render(){
     return (this.state.loading ? <div>Loading...</div> : (
       <div>
-          <div>
-            { this.state.isShowing ? <div onClick={this.closeModalHandler} className="back-drop"></div> : null }
-
-            <button className="open-modal-btn" onClick={this.openModalHandler}>Open Modal</button>
-          <div id="titleHeader">
-            <h1>Flash Cards</h1>
-          </div>
+        <div>
+            <div id="titleHeader">
+              <h1>Flash Cards</h1> 
+              <input type="button" value="Add New" onClick={() => this.openModal()} />
+            </div>
             <div className="container-fluid d-flex justify-content-around">
                 <BuildCardStack data={this.state.flashCards}/>
             </div> 
-
-                <Modal
-                    className="modal"
-                    show={this.state.isShowing}
-                    close={this.closeModalHandler}>
-                        Maybe aircrafts fly very high because they don't want to be seen in plane sight?
+        </div>
+        <div>
+                <Modal visible={this.state.visible} width="400" height="300" effect="fadeInUp" onClickAway={() => this.closeModal()}>
+                    <div>
+                        <h1>Title</h1>
+                        <p>Some Contents</p>
+                        <a href="javascript:void(0);" onClick={() => this.closeModal()}>Close</a>
+                    </div>
                 </Modal>
-            </div>
+        </div>
       </div>
       )
     )
--- src/components/modal.js
+++ src/components/modal.js
@@ -21,7 +21,7 @@ const modal = (props) => {
                 </div>
                 <div className="modal-footer">
                     <button className="btn-cancel" onClick={props.close}>CLOSE</button>
-                    <button className="btn-continue">CONTINUE</button>
+                    <button className="btn-continue">Submit</button>
                 </div>
             </div>
         </div>
